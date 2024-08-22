f2528e163148932ad511ea282a5a44ab597d96ad
Yaqeen Mumuni <yaqeens@Yaqeens-Mac.local>
2024-08-22 00:56:43 +0000

    Complete Login Responsiveness

--- frontend/src/components/signup.jsx
+++ frontend/src/components/signup.jsx
@@ -10,39 +10,39 @@ const SignUp = ({ toggle, isOpen }) => {
   return (
     <div>
       {isOpen ? (
-        <div className="form xl:my-[88px] bg-secondary-300 w-fit h-fit xl:py-[20vh] py-[2vh] px-5 align-middle xl:ml-[105px]">
+        <div className="form xl:my-[88px] bg-secondary-300 w-fit h-fit xl:py-[10vh] md:py-[5vh] py-[2vh] px-5 align-middle xl:ml-[50px] shadow-2dp rounded-lg">
           <form onSubmit={btnFunction} className="flex flex-col items-center font-roboto">
             <input
               type="email"
               placeholder="Enter email here..."
-              className="xl:placeholder:text-label md:placeholder:text-tablet-label sm:placeholder:text-mobile-label xl:w-[456px] md:w-[330px] xl:h-16 md:h-8 py-8 pl-4 rounded-lg my-4"
+              className="xl:placeholder:text-label md:placeholder:text-tablet-label placeholder:text-mobile-label xl:w-[456px] md:w-[330px] w-[220px] xl:h-16 md:h-8 h-4 py-5 md:py-6 xl:py-8 pl-4 rounded-lg md:my-4 xl:my-4 my-2"
               required
             />
             <input
               type="text"
               placeholder="Enter username here..."
-              className="xl:placeholder:text-label md:placeholder:text-tablet-label sm:placeholder:text-mobile-label xl:w-[456px] md:w-[330px] xl:h-16 md:h-8 py-8 pl-4 rounded-lg my-4"
+              className="xl:placeholder:text-label md:placeholder:text-tablet-label placeholder:text-mobile-label xl:w-[456px] md:w-[330px] w-[220px] xl:h-16 md:h-8 h-4 py-5 md:py-6 xl:py-8 pl-4 rounded-lg md:my-4 xl:my-4 my-2"
               required
             />
             <input
               type="text"
               placeholder="Enter password here..."
-              className="xl:placeholder:text-label md:placeholder:text-tablet-label sm:placeholder:text-mobile-label xl:w-[456px] md:w-[330px] xl:h-16 md:h-8 py-8 pl-4 rounded-lg my-4"
+              className="xl:placeholder:text-label md:placeholder:text-tablet-label placeholder:text-mobile-label xl:w-[456px] md:w-[330px] w-[220px] xl:h-16 md:h-8 h-4 py-5 md:py-6 xl:py-8 pl-4 rounded-lg md:my-4 xl:my-4 my-2"
               required
             />
             <input
               type="text"
               placeholder="Confirm password"
-              className="xl:placeholder:text-label md:placeholder:text-tablet-label sm:placeholder:text-mobile-label xl:w-[456px] md:w-[330px] xl:h-16 md:h-8 py-8 pl-4 rounded-lg my-4"
+              className="xl:placeholder:text-label md:placeholder:text-tablet-label placeholder:text-mobile-label xl:w-[456px] md:w-[330px] w-[220px] xl:h-16 md:h-8 h-4 py-5 md:py-6 xl:py-8 pl-4 rounded-lg md:my-4 xl:my-4 my-2"
               required
             />
             <Button
               btnName={"Create Account"}
               btnType={"submit"}
             />
-            <p className="text-tertiary-300 sm:text-mobile-label md:text-tablet-label xl:text-label">
+            <p className="text-tertiary-300 xl:text-label md:text-tablet-label text-mobile-label ">
               Already have an account?
-              <button onClick={toggle} className="text-primary-400">Click here</button>{" "}
+              <button onClick={toggle} className="text-primary-400 xl:text-label md:text-tablet-label text-mobile-label">Click here</button>{" "}
               to create one
             </p>
           </form>
--- frontend/src/pages/signIn.jsx
+++ frontend/src/pages/signIn.jsx
@@ -18,20 +18,20 @@ const SignIn = () => {
   }
   return (
     <div className="login font-comic">
-      <div className="flex flex-col xl:flex-row xl:mx-[10vw] h-[100vh]">
-        <div className="info flex xl:flex-col px-16 items-center bg-secondary-100 h-[55vh]">
-          <div className="mb-4">
-            <h1 className="text-primary-400 sm:text-mobile-display md:text-tablet-display xl:text-display">
+      <div className="flex flex-col xl:flex-row xl:mx-[10vw] h-[100vh] xl:items-center">
+        <div className="info flex flex-col md:flex-row xl:flex-col px-8 items-center bg-secondary-100 xl:bg-secondary-200 xl:h-fit h-[55vh]">
+          <div className="mb-4 md:mr-5 text-center xl:text-start ">
+            <h1 className="text-primary-400 text-mobile-display md:text-tablet-display xl:text-display">
               SafePlay <span className="text-primary-100">Learn</span>
             </h1>
             <h2 className="sm:text-mobile-title md:text-tablet-title xl:text-title text-tertiary-300">
               Where Fun Meets Learining Safely!
             </h2>
           </div>
-          <img className="xl:w-[499px] md:w-[400px] lg:w-[488px] sm:w-[348px]" src={illustration} />
+          <img className="xl:w-[499px] xl:h-fit md:w-[400px] md:h-fit lg:w-[488px] w-[348px] h-[230px]" src={illustration} />
         </div>
         {/* display login form as default and signup form on button click */}
-        <div className="form flex justify-around -mt-20">
+        <div className="form flex justify-around xl:justify-normal -mt-10 md:-mt-20 xl:mt-0">
           {isSignUp ? (
             <SignUp isOpen={isSignUp} toggle={()=>toggle(isSignUp)} />
           ) : (
