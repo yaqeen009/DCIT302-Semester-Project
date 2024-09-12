945b4b7ec5f7251bb3fe51fa4631c585a3393c6b
IddrisTipagya <iddristipagya7@gmail.com>
2024-09-12 17:12:35 +0000

    singin/signup page tablet view

--- frontend/src/pages/settings.jsx
+++ frontend/src/pages/settings.jsx
@@ -5,7 +5,9 @@ const Settings = () => {
   return (
     <div className="settings font-comic z-0 flex flex-col md:flex-row bg-secondary-200">
       {/* For mobile view, headings on top */}
+      
       <div className="space-y-8 md:hidden  bg-primary-200 p-4">
+        
         <h1 className="text-5xl text-danger-200 mb-4 text-center">Settings</h1>
         <h2 className="text-2xl text-danger-200 mb-4 text-center">Account Settings</h2>
       </div>
--- frontend/src/pages/signIn.jsx
+++ frontend/src/pages/signIn.jsx
@@ -7,35 +7,42 @@ const SignIn = () => {
   const [isLogIn, setIsLogIn] = useState(true);
   const [isSignUp, setIsSignUp] = useState(false);
 
-  const toggle =(state) => {
+  const toggle = (state) => {
     if (state === isLogIn) {
-      setIsLogIn(false)
-      setIsSignUp(true)
-    }else{
-      setIsLogIn(true)
-      setIsSignUp(false)
+      setIsLogIn(false);
+      setIsSignUp(true);
+    } else {
+      setIsLogIn(true);
+      setIsSignUp(false);
     }
-  }
+  };
+
   return (
-    <div className="login font-comic">
-      <div className="flex flex-col xl:flex-row xl:mx-[10vw] h-[100vh] xl:items-center">
-        <div className="info flex flex-col md:flex-row xl:flex-col px-8 items-center bg-secondary-100 xl:bg-secondary-200 xl:h-fit h-[55vh]">
-          <div className="mb-4 md:mr-5 text-center xl:text-start ">
+    <div className="login font-comic bg-secondary-200 overflow-auto">
+      <div className="flex flex-col xl:flex-row h-[100vh] xl:mx-[10vw] xl:items-center">
+        {/* Info section */}
+        <div className="info flex flex-col md:flex-row xl:flex-col px-8 items-center bg-secondary-100 xl:bg-secondary-200 xl:h-fit h-[55vh] w-full xl:w-[1024px]">
+          <div className="mb-4 md:mr-5 text-center xl:text-start">
             <h1 className="text-primary-400 text-mobile-display md:text-tablet-display xl:text-display">
-              SafePlay <span className="text-primary-100">Learn</span>
+              SafePlay <span className="text-[#6be059]">Learn</span>
             </h1>
-            <h2 className="sm:text-mobile-title md:text-tablet-title xl:text-title text-tertiary-300">
-              Where Fun Meets Learining Safely!
+            <h2 className="text-tertiary-300 text-sm md:text-base xl:text-lg">
+              Where Fun Meets Learning Safely!
             </h2>
           </div>
-          <img className="xl:w-[499px] xl:h-fit md:w-[400px] md:h-fit lg:w-[488px] w-[348px] h-[230px]" src={illustration} />
+          <img
+            className="xl:w-[499px] md:w-[400px] w-[348px] h-auto"
+            src={illustration}
+            alt="Illustration"
+          />
         </div>
-        {/* display login form as default and signup form on button click */}
-        <div className="form flex justify-around xl:justify-normal -mt-10 md:-mt-20 xl:mt-0">
+
+        {/* Form section */}
+        <div className="form flex justify-around xl:justify-normal mt-8 md:mt-0 xl:w-[474.3px]  rounded-lg p-4">
           {isSignUp ? (
-            <SignUp isOpen={isSignUp} toggle={()=>toggle(isSignUp)} />
+            <SignUp isOpen={isSignUp} toggle={() => toggle(isSignUp)} />
           ) : (
-            <Login isOpen={isLogIn} toggle={()=>toggle(isLogIn)} />
+            <Login isOpen={isLogIn} toggle={() => toggle(isLogIn)} />
           )}
         </div>
       </div>
